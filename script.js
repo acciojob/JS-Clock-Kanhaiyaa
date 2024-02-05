7237172cb1962f0d2283bde7d32c25f2174e15a1
Kanhaiyaa <128280779+Kanhaiyaa@users.noreply.github.com>
2024-02-05 16:01:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your code here
+
+const hourHand = document.querySelector('.hour-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const secondHand = document.querySelector('.second-hand');
+
+function rotateClockHands(){
+    const now = new Date();
+    const hour =now.getHours();
+    const minute = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    const hourDegree = ((hour %12) +minute/60)*30;
+    const minuteDegree = ((minute + seconds)/60)*6;
+    const secondDegree = seconds*6;
+
+    hourHand.style.transform ='rotate(${hourDegree}deg)';
+    minuteHand.style.transform = 'rotate(${minuteDegree}deg)';
+    secondHand.style.transform = 'rotate(${secondDegree}deg)';
+}
+
+setInterval(rotateClockHands,1000)
+
+rotateClockHands()
